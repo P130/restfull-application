251e02d3bf367a8e2666f69545cae24c2be17ac7
Stathis Kapsiotis <kakobato@yahoo.gr>
2019-04-16 15:57:52 +0300

    Add files via upload

---
+++ ziggy/rest/webservices/restfulwebservices/config/Swagger2Config.java
@@ -0,0 +1,35 @@
+package com.ziggy.rest.webservices.restfulwebservices.config;
+
+
+public class Swagger2Config {
+	
+//	@Bean
+//	public Docket api() {
+//		return new Docket(DocumentationType.SWAGGER_2);
+//	}
+	
+
+//    @Bean
+//    public Docket api2() {
+//        return new Docket(DocumentationType.SWAGGER_2).select()
+//            .apis(RequestHandlerSelectors
+//            .basePackage("net.guides.springboot2.springboot2swagger2.controller"))
+//            .paths(PathSelectors.regex("/.*"))
+//            .build().apiInfo(apiEndPointsInfo());
+//
+//    }
+    
+//    private ApiInfo apiEndPointsInfo() {
+//        return new ApiInfoBuilder().title("Spring Boot REST API")
+//            .description("Employee Management REST API")
+//            .contact(new Contact("Ramesh Fadatare", "www.javaguides.net", "ramesh24fadatare@gmail.com"))
+//            .license("Apache 2.0")
+//            .licenseUrl("http://www.apache.org/licenses/LICENSE-2.0.html")
+//            .version("1.0.0")
+//            .build();
+//
+//    }
+	
+}
+
+
